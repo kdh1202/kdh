32ba3d6455010cdd4358c5c41122ce737b03691a
김동현 <ehdgus951202@naver.com>
2025-05-13 17:31:13 +0900

    5월13일 종료

--- workspace_sql/오라클.sql
+++ workspace_sql/오라클.sql
@@ -20,6 +20,10 @@ sal,
 sal*12+comm s, sal*12+comm ,
 sal*12+comm "s  s",-- sql에서 유일하게 "가 들어가는곳
 comm from emp;
+
+select  empno "사원", ename "이름", job"직업" 
+from emp;
+
 select * from emp order by sal desc;
 select * from emp order by deptno asc, sal desc;
 
@@ -39,9 +43,98 @@ order by ename;
 
 select * from emp order by deptno, sal desc;
 --deptno, sal desc; 이상황에서 연봉이 같으면 사원번호 내림차순으로 정렬
-
 --부서번호로 오름차순, 겹치면 sal 내림차순
 
 select * from emp order by deptno, sal desc, empno desc;
 
+select distinct deptno from emp;
+
+select * from emp where deptno = 30
+order by sal;
+
+select * from emp where sal = 1600;   -- 같다
+select * from emp where sal > 1600;    -- 크다 
+select * from emp where sal < 1600;   -- 작다 
+select * from emp where sal >= 1600;  -- =은 항상 오른쪽
+select * from emp where deptno != 30;   --  !은 부정
+select * from emp where deptno <> 30;  -- 부정
+
+select * from emp where deptno = 30 and job = 'SALESMAN';
+
+--or
+/*
+ emp 테이블에서 부서번호(deptno)가 30 이거나(또는) 
+ 직책(job)이 clerk 인 사원의  모든정보를 출력해라
+*/
+-- and와 or 들이 있을떄 and 먼저 적용된다
+select * from emp where deptno = 30 or job = 'CLERK';
+
+
+/*
+job이 CLERK sal이 2000초과 이거나 deptno 10인 사원 조회
+*/
+select * from emp where (job = 'CLERK' or sal > 2000) and deptno =10;
+
+select * from emp where job = 'CLERK' or sal > 2000 and deptno =10;
+
+select * from emp where job = 'CLERK' or (sal > 2000 and deptno=10);
+
+-- ()먼저 실행해야 한는 것을 묶어 주는게 편함
+
+select * from emp where not (job = 'CLERK' or ( sal > 2000 and deptno = 10 ) );
+
+/*
+    deptno != 30, deptno <> 30, deptno ^=30, not deptno = 30
+*/
+
+--월급2000만원이상 4000미만인 사월을 출력하시오
+select * from emp where sal >= 2000 and sal < 4000;
+
+-- 2000 <= sal < 4000;
+-- 2000 <= sal and sal < 4000;
+
+--  부서번호 10 또는 20인 사원만 출력
+
+select * from emp where deptno = 10 or deptno =20;
+select * from emp where deptno in ( 10, 20 );
+
+-- 부서번호 10 또는 20인 사원뺴고 출력
+select * from emp where deptno not in ( 10, 20);
+
+
+select * from emp where empno in ( 7369, 7934 );
+select * from emp where empno in ( 10, 20 );
+
+
+
+select *  from emp where job > 'B' and job < 'Z';
+
+select * from emp order by job desc;
+
+select * from emp where deptno = 30;
+
+select * from emp where deptno =10 or deptno = 20;
+
+select * from emp where mgr >= 7500 or mgr = 7999;
+select * from emp where mgr = 7698 and job = 'SALESMAN';
+select * from emp where ename = 'SCOTT' and job = 'ANALYST';
+
+
+select * from emp where sal >= 1600 or sal = 4000 and  comm = 300;
+
+select * from emp where sal <3000;
+/*
+select * from emp where  sal != 3000;
+select * from emp where  sal ^= 3000;
+select * from emp where  sal <> 3000;
+뜻 = 같지않다*/
+
+select * from emp where  job not in ('SALESMAN', 'MANAGER', 'CLERK');
+select * from emp where job <> 'SALESMAN';
+select * from emp where job != 'SALESMAN' and job not in ('MANAGER', 'CLERK',
+'PRESIDENT');
+
+select * from emp where sal >= 1000 and sal <= 3000;
+
+select * from emp where mgr > 7600 and mgr < 7900;
 
